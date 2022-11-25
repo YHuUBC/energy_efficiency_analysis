8faa307c5bb063ea764d0ac13f2e455ce264262e
meh2307 <mehwishbhat.ubc@gmail.com>
2022-11-25 13:40:51 -0800

    changed output from excel file to csv

--- src/download_data.py
+++ src/download_data.py
@@ -3,7 +3,7 @@
 # date: 2019-12-18
 ## code to download
 # python src/download_data.py --url='http://archive.ics.uci.edu/ml/machine-learning-databases/00242/ENB2012_data.xlsx' 
-#--out_file=data/raw/ENB2012_data.xlsx
+#--out_file=data/raw/ENB2012_data.csv
 """Downloads data excel data from the web to a local filepath in excel file format.
 Usage: src/down_data.py  --url=<url> --out_file=<out_file>
 Options:
@@ -30,10 +30,10 @@ def main(url, out_file):
   data = pd.read_excel(url, header=0)
   
   try:
-      data.to_excel(out_file, index = False)
+      data.to_csv(out_file, index = False)
   except:
       os.makedirs(os.path.dirname(out_file))
-      data.to_excel(out_file, index = False)
+      data.to_csv(out_file, index = False)
 
 if __name__ == "__main__":
   main( opt["--url"], opt["--out_file"])
